10012a9f380b5004f33595d8e38bccd35dde492e
yogesh-akashi <floydianyogesh07@gmail.com>
2025-04-05 21:57:43 +0545

    uodate index.html

--- script.js
+++ script.js
@@ -2,11 +2,13 @@ const hamburger = document.querySelector(".hamburger");
 const bar1 = document.querySelector(".bar1");
 const bar2 = document.querySelector(".bar2");
 const bar3 = document.querySelector(".bar3");
+const mobileNav = document.querySelector(".mobileNav");
 
 hamburger.addEventListener("click", function () {
   bar1.classList.toggle("animateflipBar1");
   bar2.classList.toggle("animatefadeBar2");
   bar3.classList.toggle("animateflipBar3");
+  mobileNav.classList.toggle("openMobileNav");
 });
 console.log(hamburger);
 console.log("hello");
